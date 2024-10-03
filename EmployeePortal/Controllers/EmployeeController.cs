8c103b9bc067775d9fca77e8f8c15dcbbd0ee380
EvanMworia <evannji99@gmail.com>
2024-10-03 14:09:41 +0300

    CRUD operations working

--- EmployeePortal/Controllers/EmployeeController.cs
+++ EmployeePortal/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using EmployeePortal.Data;
 using EmployeePortal.Models.DTO_s;
+using EmployeePortal.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,21 +18,104 @@ namespace EmployeePortal.Controllers
             this._dbContext = dbContext;
             _responseDTO = new ResponseDTO();
         }
+        [HttpPost]
+        public IActionResult AddNewEmployee(EmployeeDTO newEmployeeDetails)
+        {
+            var employee = new Employee()
+            {
+                Name = newEmployeeDetails.Name,
+                Email = newEmployeeDetails.Email,
+                Phone = newEmployeeDetails.Phone,
+                Salary = newEmployeeDetails.Salary,
+            };
+            _dbContext.Add(employee);
+            _dbContext.SaveChanges();
+
+            _responseDTO.Message = $"Employee '{employee.Name}' has been created successfully";
+            _responseDTO.Result = employee;
+
+            return Ok(_responseDTO);
+        }
         [HttpGet]
         public ActionResult GetAllEmployees()
         {
-            //var _responseDTO = new ResponseDTO();
-            var allEmployees = _dbContext.Employees.ToList();
-            if (allEmployees.Count == 0)
+            try
             {
-                _responseDTO.Message = "No Employees Found";
-                _responseDTO.Result = allEmployees;
-                return NotFound(_responseDTO);
-            }
-            
-            return Ok(allEmployees);
 
-            
+                var allEmployees = _dbContext.Employees.ToList();
+                if (allEmployees.Count == 0)
+                {
+                    _responseDTO.Message = "No Employees Found, Add New Employees";
+                    _responseDTO.Result = allEmployees;
+                    return Ok(_responseDTO);
+                }
+
+                return Ok(allEmployees);
+            }
+            catch (Exception ex)
+            {
+
+                _responseDTO.Message = ex.Message;
+
+                return BadRequest(_responseDTO);
+            }
+
+
+
         }
+        [HttpGet]
+        [Route("SingleEmployee/{id:guid}")]
+        public ActionResult GetEmployeeById(Guid id)
+        {
+            var employee = _dbContext.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+
+        }
+        [HttpPut]
+        [Route("UpdateEmployee/{id:guid}")]
+        public IActionResult UpdateEmployeeDetails(Guid id, EmployeeDTO updatedEmployeeDetails)
+        {
+
+            var employee = _dbContext.Employees.Find(id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+            employee.Name = updatedEmployeeDetails.Name;
+            employee.Email = updatedEmployeeDetails.Email;
+            employee.Phone = updatedEmployeeDetails.Phone;
+            employee.Salary = updatedEmployeeDetails.Salary;
+            _dbContext.SaveChanges();
+
+            _responseDTO.Message = $"'{employee.Id}' HAS BEEN UPDATED SUCCESFULLY";
+            _responseDTO.Result = employee;
+            return Ok(_responseDTO);
+
+        }
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public ActionResult DeleteEmployee(Guid id)
+        {
+            var employee = _dbContext.Employees.Find(id);
+            if(employee is null)
+            {
+                return NotFound();
+            }
+            _dbContext.Employees.Remove(employee);
+            _dbContext.SaveChanges();
+
+            _responseDTO.Message = $" The Entity '{employee.Id}' has been deleted";
+            
+            return Ok(_responseDTO);
+        }
+
+        
+
+
     }
+    
 }
